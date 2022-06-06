0db6c4984091d7d92961465d9032a2138bad77eb
weiy1 <weiy1@LEGAL.REGN.NET>
2022-06-06 17:05:21 +0800

    added stock 122/123 solutions in dp

--- DP/best_time_to_buy_and_sell_stock.py
+++ DP/best_time_to_buy_and_sell_stock.py
@@ -1,8 +1,10 @@
 """
+https://github.com/labuladong/fucking-algorithm/blob/master/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E7%B3%BB%E5%88%97/%E5%9B%A2%E7%81%AD%E8%82%A1%E7%A5%A8%E9%97%AE%E9%A2%98.md
+
 股票问题，考虑三个因素:
 1. 第几天
 2. 最高交易上限几次
-3. 手中股票状态，持有/不持有/冷冻期
+3. 手中股票状态，持有/不持有
 
 
 base case：
@@ -45,3 +47,28 @@ class Solution:
             dp_0 = max(dp_0, dp_1+prices[i])
             dp_1 = max(temp-prices[i], dp_1)
         return dp_0
+
+# 122使用贪心算法会更高效
+# 因为不限制买卖次数，凡是当前价格比前一天高，就可以在前一天买入今天卖出
+class Solution:
+    def maxProfit(self, prices) -> int:
+        res = 0
+        for i in range(1,len(prices)):
+            if prices[i] > prices[i-1]:
+                res += prices[i] - prices[i-1]
+        return res
+
+
+# 123 多了交易次数的限制，需要穷举交易次数
+class Solution:
+    def maxProfit(self, prices) -> int:
+        dp = [[[0,0] for _ in range(3)] for _ in range(len(prices))]
+        for i in range(len(prices)):
+            for j in range(1,3):
+                if i == 0:
+                    dp[i][j][0] = 0
+                    dp[i][j][1] = -prices[i]
+                    continue
+                dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1]+prices[i])
+                dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0]-prices[i])
+        return dp[-1][2][0]
