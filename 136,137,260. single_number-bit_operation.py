50aad3aa870cac9294085679efd87b95ff449de0
weiy1 <weiy1@LEGAL.REGN.NET>
2022-06-07 22:57:21 +0800

    single number 137 solution note added

--- 136,137,260. single_number-bit_operation.py
+++ 136,137,260. single_number-bit_operation.py
@@ -46,6 +46,8 @@ class Solution:
 
         num1, num2 = 0, 0
         # 通过与操作判断lowb位是否为1，来分流nums变成两个子数组，分别做异或，得到两个不一样的数字
+        # 与运算,1只有和1与才不为0，0和什么与都为0
+        # lowb除了最低二进制位为1其他位都为0，也就是其他位无论为什么最终都是0，只有最低二进制位为1的时候整个数字不为0，这是最快区分的方法
         for num in nums:
             if num & lowb == 0:
                 num1 ^= num
@@ -61,13 +63,13 @@ class Solution:
 
 # 使用两位二进制数字来扫描每一个数字的第i位（计算时实际上是同时扫描所有位）
 # https://leetcode.cn/problems/single-number-ii/solution/single-number-ii-mo-ni-san-jin-zhi-fa-by-jin407891/
-class Solution:
-    def singleNumber(self, nums) -> int:
-        ones, twos = 0, 0
-        for num in nums:
-            ones = ones ^ num & ~twos
-            twos = twos ^ num & ~ones
-        return ones
+# class Solution:
+#     def singleNumber(self, nums) -> int:
+#         ones, twos = 0, 0
+#         for num in nums:
+#             ones = ones ^ num & ~twos
+#             twos = twos ^ num & ~ones
+#         return ones
 
 if  __name__ == '__main__':
     Solution().singleNumber([1,2,1,3,2,5])
