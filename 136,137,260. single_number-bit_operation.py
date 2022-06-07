63868e6ea9f2503b921940aebbbb3265fc4f3752
weiy1 <weiy1@LEGAL.REGN.NET>
2022-06-07 22:27:14 +0800

    single number 137 solution

--- 136,137,260. single_number-bit_operation.py
+++ 136,137,260. single_number-bit_operation.py
@@ -1,4 +1,7 @@
 """
+位运算：
+https://www.runoob.com/w3cnote/bit-operation.html
+
 异或运算:
 参加运算的两个数据，按二进制位进行“异或”运算。
 运算规则：0^0=0；   0^1=1；   1^0=1；   1^1=0；
@@ -9,9 +12,7 @@
 参加运算的两个数据，按二进制位进行“与”运算。
 运算规则：0&0=0;   0&1=0;    1&0=0;     1&1=1;
 即：两位同时为“1”，结果才为“1”，否则为0
-"""
 
-"""
 a & (-a) 可以获得a最低的非0位 比如a的二进制原码是 0000 1010，这里最低非0位是bit 1（从右往左第2位）
 -a在二进制中的表示是补码(2's complement code)形式，即先按位取反再加1
 取反得 1111 0101(即1's complement code，反码)
@@ -23,8 +24,18 @@ a & (-a) 可以获得a最低的非0位 比如a的二进制原码是 0000 1010，
 原最低非0位是1，取反后是0，进位到这一位0变成1，不再向左进位
 原最低非0位左边的每一位经由取反后 和 原码 进行与运算必为0
 """
-# 260
 
+
+# 136
+class Solution:
+    def singleNumber(self, nums):
+        _sum = 0
+        for num in nums:
+            _sum ^= num
+        return _sum
+
+
+# 260
 class Solution:
     def singleNumber(self, nums):
         _sum = 0
@@ -42,6 +53,21 @@ class Solution:
                 num2 ^= num
 
         return [num1, num2]
+
+
+# 137
+# 考虑数字的二进制形式，对于出现三次的数字，各 二进制位 出现的次数都是 3 的倍数。
+# 因此，统计所有数字的各二进制位中 1 的出现次数，并对 3 求余，结果则为只出现一次的数字。
+
+# 使用两位二进制数字来扫描每一个数字的第i位（计算时实际上是同时扫描所有位）
+# https://leetcode.cn/problems/single-number-ii/solution/single-number-ii-mo-ni-san-jin-zhi-fa-by-jin407891/
+class Solution:
+    def singleNumber(self, nums) -> int:
+        ones, twos = 0, 0
+        for num in nums:
+            ones = ones ^ num & ~twos
+            twos = twos ^ num & ~ones
+        return ones
 
 if  __name__ == '__main__':
     Solution().singleNumber([1,2,1,3,2,5])
