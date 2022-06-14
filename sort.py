0aca5b1722c9cb010ec2b6c3e2e26d30849bf764
weiy1 <weiy1@LEGAL.REGN.NET>
2022-06-14 09:17:06 +0800

    added quicksort solution

---
+++ sort.py
@@ -0,0 +1,23 @@
+# quick sort
+class Solution:
+    def findKthLargest(self, nums, k) -> int:
+        def quick_sort(begin, end, nums):
+            if begin >= end:
+                return nums
+            i,j = begin,end
+            pivot = nums[i]
+            while i < j:
+                while i < j and nums[j] >= pivot:
+                    j -= 1
+                nums[i] = nums[j]
+                while i < j and nums[i] <= pivot:
+                    i += 1
+                nums[j] = nums[i]
+            nums[j] = pivot
+            quick_sort(begin, i-1, nums)
+            quick_sort(i+1, end, nums)
+            return nums
+        return quick_sort(0, len(nums)-1, nums)[k-1]
+
+if __name__ == '__main__':
+    print(Solution().findKthLargest([3,2,1,5,6,4], 2))
