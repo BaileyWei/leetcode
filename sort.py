f61c29b03789cf194fb1a131fd174839abd87632
weiy1 <weiy1@LEGAL.REGN.NET>
2022-06-15 10:29:03 +0800

    quiksort random

--- sort.py
+++ sort.py
@@ -23,28 +23,5 @@ class Solution:
             return nums
         return quick_sort(0, len(nums)-1, nums)[k-1]
 
-class Solution:
-    def sortArray(self, nums):
-        import random
-        def quickSort(begin, end, nums):
-            if begin > end:
-                return nums
-            i,j = begin,end
-            rand = random.randint(begin, end)
-            pivot = nums[rand]
-            nums[i], nums[rand] = nums[rand], nums[i]
-            while i < j:
-                while i < j and nums[j] >= pivot:
-                    j -= 1
-                nums[i] = nums[j]
-                while i < j and nums[i] <= pivot:
-                    i += 1
-                nums[j] = nums[i]
-            nums[j] = pivot
-            quickSort(begin, j-1, nums)
-            quickSort(j+1, end, nums)
-            return nums
-        return quickSort(0, len(nums)-1, nums)
-
 if __name__ == '__main__':
     print(Solution().sortArray([3,2,1,5,6,4]))
